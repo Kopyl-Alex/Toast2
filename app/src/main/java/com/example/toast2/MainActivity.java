692187bf7821c48d495142063442cde818d2f490
Alex Kopyl <kopyl@mail.ru>
2020-01-21 12:46:23 +0200

    111

--- app/src/main/java/com/example/toast2/MainActivity.java
+++ app/src/main/java/com/example/toast2/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.toast2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
-    public void onClick(View view) {
 
+    public void showToast(View view) {
+        Toast toast3 = Toast.makeText(getApplicationContext(),
+                R.string.catfood, Toast.LENGTH_LONG);
+        toast3.setGravity(Gravity.CENTER, 0, 0);
+        LinearLayout toastContainer = (LinearLayout) toast3.getView();
+        ImageView catImageView = new ImageView(getApplicationContext());
+        catImageView.setImageResource(R.drawable.dnei);
+        toastContainer.addView(catImageView, 0);
+        toastContainer.setBackgroundColor(Color.TRANSPARENT);
+        toast3.show();
     }
+}
